b682391c20bb2d728d7947bb842b7c8bce4e2058
Julien Stroheker <julienstroheker@gmail.com>
2017-10-17 12:07:16 -0400

    Update driver_install.sh

--- driver_install.sh
+++ driver_install.sh
@@ -8,7 +8,7 @@ wget -P /tools http://us.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-
 
 chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
 
-sudo wget https://raw.githubusercontent.com/wbuchwalter/acs-k8s-gpu/master/nvidia_dev.bash -O /etc/init.d/nvidia
+sudo wget https://raw.githubusercontent.com/julienstroheker/acs-dcos-gpu/master/nvidia_dev.bash -O /etc/init.d/nvidia
 sudo chmod +x /etc/init.d/nvidia
 sudo update-rc.d nvidia defaults
 sudo service nvidia start
