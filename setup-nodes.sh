332b0086110558d8ffabd342be3fd337422a461b
Julien Stroheker <julienstroheker@gmail.com>
2017-10-17 12:24:12 -0400

    Delete setup-nodes.sh

--- setup-nodes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-nodes=$(kubectl get node -o name | grep -o 'k8s-agentpool[0-9]-[0-9]*-[0-9]')
-
-for node in $nodes; do
-        ssh -t -oStrictHostKeyChecking=no $node "echo 'Installing driver on $node...' && curl -L -sf https://raw.githubusercontent.com/ritazh/acs-k8s-gpu/master/install-nvidia-driver.sh | sudo sh "
-done
